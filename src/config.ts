55974a421cc7396da873cbbc3b5c0f65622b5ecd
Quasi2317 <142194632+hqslsz@users.noreply.github.com>
2026-01-27 16:56:59 +0800

    feat: 更新背景图、加载动画及TOC样式
    - config: 更换背景图和加载动画资源
    - Footer: 修改版权信息颜色为白色
    - HeroBanner: 调整社交链接图标大小，优化文本样式
    - TOC: 更新样式以适应新设计，调整深度标记颜色
    - HomeLayout: 优化TOC显示逻辑，确保在HeroBanner可见时隐藏TOC
    - Layout: 改进背景图加载逻辑，减少闪烁现象
    - MainGridLayout: 调整TOC组件位置和样式以提升用户体验

--- src/config.ts
+++ src/config.ts
@@ -41,7 +41,7 @@ export const siteConfig: SiteConfig = {
 
 	background: {
 		enable: true, // 是否启用背景图
-		src: "https://morbol.qus.moe/bg/classroom.gif", // 随机背景API（已添加缓存机制防止闪烁）
+		src: "https://morbol.qus.moe/bg/startrain.gif", // 随机背景API（已添加缓存机制防止闪烁）
 		position: "center", // 背景位置: 'top', 'center', 'bottom'
 		size: "cover", // 背景大小: 'cover', 'contain', 'auto'
 		repeat: "no-repeat", // 背景重复: 'no-repeat', 'repeat'
@@ -52,7 +52,7 @@ export const siteConfig: SiteConfig = {
 	// 加载动画配置
 	loadingScreen: {
 		enable: true, // 是否启用加载动画
-		src: "https://morbol.qus.moe/loading/bocchi_loading2.mp4", // 加载动画资源（支持图片/视频）
+		src: "https://morbol.qus.moe/loading/creeper_loading.gif", // 加载动画资源（支持图片/视频）
 		type: "auto", // 资源类型: 'image', 'video', 'auto'（自动识别）
 		text: "少女祈祷中...", // 加载提示文字
 		minDuration: 3000, // 最小显示时间（毫秒）：5秒 = 5000ms
