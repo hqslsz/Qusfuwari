ab9c2e6bad5ec5fa637f8a35a35c32204d62cc26
Quasi2317 <wlrh_lishuzeWR@outlook.com>
2026-02-22 18:06:51 +0800

    chore: update dependencies and configuration
    - Added .pnpm-store/ to .gitignore to exclude PNPM store files.
    - Included "node-gyp" as a dependency in package.json with version ^12.2.0.
    - Updated pnpm-lock.yaml to reflect changes in package resolutions and added encoding@0.1.13 for various packages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -141,7 +141,7 @@ export default defineConfig({
 			parseDirectiveNode,
 		],
 		rehypePlugins: [
-			rehypeKatex,
+			[rehypeKatex, { strict: false }],
 			rehypeSlug,
 			[
 				rehypeComponents,
